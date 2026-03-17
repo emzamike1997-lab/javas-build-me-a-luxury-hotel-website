0bdcd3b47e4e44ac8b1626ef044aa9f78ae5a851
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-17 11:16:55 +0100

    JAVAS: Add src/components/Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+```javascript
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2024 Luxury Hotel</p>
+        </footer>
+    );
+}
+
+export default Footer;
+```
+
+####
