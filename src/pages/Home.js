e12403193c0aece8a9d1abcc0808355629c37ed5
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-17 11:16:56 +0100

    JAVAS: Add src/pages/Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+```javascript
+import React from 'react';
+
+function Home() {
+    return (
+        <main>
+            <h1>Welcome to Luxury Hotel</h1>
+            <p>We offer the finest accommodations and exceptional service.</p>
+        </main>
+    );
+}
+
+export default Home;
+```
+
+####
